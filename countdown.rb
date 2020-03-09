e8c26195064120b9507fc67ffc91e339b62712f1
Camisha  Farris <cfarris@anointedflooring.com>
2020-03-09 20:03:55 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
-#write your code here
 
-def countdown
+def countdown(seconds)
+  while seconds > 0
+    puts "#{seconds} second(s)"
+    seconds -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
+
+
+def countdown_with_sleep(seconds)
+   while seconds > 0
+    puts "#{seconds} SECOND(S)"
+    sleep 5
+    seconds -= l
+  end
+  return " HAPPY NEW YEAR"
+end  
